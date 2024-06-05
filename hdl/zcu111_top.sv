bfb302b3b44e14aafb294ab3aa3727eb131c9893
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-06-05 13:52:01 -0400

    add AGC design

--- hdl/zcu111_top.sv
+++ hdl/zcu111_top.sv
@@ -53,7 +53,7 @@ module zcu111_top(
         output [1:0] PL_USER_LED        // { AP13, AR13 }
     );
 
-   parameter	     THIS_DESIGN = "BIQUAD";
+   parameter	     THIS_DESIGN = "AGC";
    
     
     (* KEEP = "TRUE"  *)
@@ -259,6 +259,26 @@ module zcu111_top(
 
     // NOW we can have multiple designs in one top level thingy
     generate
+         if (THIS_DESIGN == "AGC") begin : AGC
+            agc_design u_design( .wb_clk_i(ps_clk),
+                                 .wb_rst_i(1'b0),
+                                 `CONNECT_WBS_IFS( wb_ , bm_ ),
+                                 .aclk(aclk),
+                                 .aresetn(1'b1),
+                                  `CONNECT_AXI4S_MIN_IF( adc0_ , adc0_ ),
+                                  `CONNECT_AXI4S_MIN_IF( adc1_ , adc1_ ),
+                                  `CONNECT_AXI4S_MIN_IF( adc2_ , adc2_ ),
+                                  `CONNECT_AXI4S_MIN_IF( adc3_ , adc3_ ),
+                                  `CONNECT_AXI4S_MIN_IF( adc4_ , adc4_ ),
+                                  `CONNECT_AXI4S_MIN_IF( adc5_ , adc5_ ),
+                                  `CONNECT_AXI4S_MIN_IF( adc6_ , adc6_ ),
+                                  `CONNECT_AXI4S_MIN_IF( adc7_ , adc7_ ),
+                                  // buffers
+                                  `CONNECT_AXI4S_MIN_IF( buf0_ , buf0_ ),
+                                  `CONNECT_AXI4S_MIN_IF( buf1_ , buf1_ ),
+                                  `CONNECT_AXI4S_MIN_IF( buf2_ , buf2_ ),
+                                  `CONNECT_AXI4S_MIN_IF( buf3_ , buf3_ ));            
+         end
          if (THIS_DESIGN == "BASIC") begin : BSC
             basic_design u_design( .wb_clk_i(ps_clk),
                                    .wb_rst_i(1'b0),
